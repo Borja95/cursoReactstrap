1d5578309f8d338d19b7b791c6b6d781379f34bc
Borja95 <abj951122@gmail>
2024-09-12 00:32:54 -0600

    video 2 Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
-import logo from './logo.svg';
+import logo from './borjascript logo.jpg';
 import './App.css';
-import { Button } from 'reactstrap';
+import { Navbar, NavbarBrand, NavbarToggler, Collapse, Nav, NavItem, NavLink } from 'reactstrap';
+import { useState } from 'react';
 
 function App() {
+
+  const [estaAbiertoNavbar, setEstaAbiertoNavbar] = useState(false);
+
+
   return (
     <div className="App">
-      <br />
-       <Button color="danger">Danger!</Button>
+      <Navbar  fixed='top' expand='sm' className='custom-navbar'>
+
+      <NavbarBrand href='https://www.youtube.com/@abj95100?sub_confirmation=1' target='blank'>
+          <img src={logo} width={30} height={30} aria-label='Logo Borjascript'/>
+          Borjascript
+
+        </NavbarBrand>
+
+        <NavbarToggler aria-label='Abrir Menú' aria-expanded={estaAbiertoNavbar} 
+        onClick={()=>setEstaAbiertoNavbar(!estaAbiertoNavbar)} />
+        <Collapse isOpen={estaAbiertoNavbar} navbar>
+        <Nav className='me-auto' navbar>
+          <NavItem>
+            <NavLink href='/home' className='custom-nav-item'>Primer Elemento</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href='/about' className='custom-nav-item'>Segundo Elemento</NavLink>
+          </NavItem>
+        </Nav>
+
+        </Collapse>
+
+        
+        
+
+
+      </Navbar>
     </div>
   );
 }
